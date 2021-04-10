d91e844bb24412360ecb5123e21bda367e0c4f07
Your Name <you@example.com>
2021-04-11 03:27:43 +0600

    created auth module: docker file, depl file, ingress-srv

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,17 @@
 import express from 'express'
-import {json} from 'body-parser'
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
-app.use(json());
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/api/users/currentuser', (req, res)=>{
+    res.send('hello there!')
+})
+app.get('/api/users/currentusers', (req, res)=>{
+    res.send('hi there!')
+})
 app.listen(3000, ()=>{
     console.log("auth port 3000")
 })
