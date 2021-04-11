b7ec6a6f00772f2326a90a43cc0137e6be5bc52a
Your Name <you@example.com>
2021-04-12 02:43:26 +0600

    error handler structure created

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,11 @@
-import express from 'express'
+import express from 'express';
+import 'express-async-errors';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-errors';
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -6,12 +13,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/api/users/currentuser', (req, res)=>{
-    res.send('hello there!')
-})
-app.get('/api/users/currentusers', (req, res)=>{
-    res.send('hi there!')
-})
-app.listen(3000, ()=>{
-    console.log("auth port 3000")
-})
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+// all means, any type of request get/post/put... if the url not found
+//then bellow code will throw a error
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+app.listen(3000, () => {
+  console.log('auth port 3000');
+});
