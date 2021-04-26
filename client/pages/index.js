342cd4b5f231f5ef66e2d5978876ed8b26e96e66
Your Name <you@example.com>
2021-04-27 00:00:05 +0600

    signin component

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import  buildClient from "../api/build-client";
 const LandingPage = ({currentUser})=>{
     console.log({currentUser})
-    return "hello"
+    return currentUser ?(<h1>You are signed in</h1>): (<h1>You are not signed in</h1>)
 }
 LandingPage.getInitialProps = async (context) =>{
     const client =  buildClient(context);
