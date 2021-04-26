82e9b3cd6d3beaf9cfb6ec389dcebaf7c8c15e09
Your Name <you@example.com>
2021-04-26 22:50:31 +0600

    created build client helper

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,29 +1,11 @@
-import  axios from "axios";
+import  buildClient from "../api/build-client";
 const LandingPage = ({currentUser})=>{
-    //axios.get('/api/users/currentuser');
     console.log({currentUser})
     return "hello"
 }
-LandingPage.getInitialProps = async () =>{
-    if (typeof window === 'undefined'){
-        console.log("getInitialProps I am on the server");
-        // we are on the server 
-        // request should be mand to ingress
-       // http://ingress-nginx.kube-system.svc.cluster.local
-       const {data} = await axios.get('http://ingress-nginx-controller.kube-system.svc.cluster.local/api/users/currentuser', {
-           headers:{
-               host: 'ticketing.com'
-           }
-        });
-       return data;
-        return {}
-    }else{
-        console.log("getInitialProps I am on the browser");
-
-        //we are on the browser
-        // request can be made with a base url of '
-        const {data} = await axios.get('/api/users/currentuser');
-        return data;
-    }
+LandingPage.getInitialProps = async (context) =>{
+    const client =  buildClient(context);
+    const {data} = await client.get('/api/users/currentuser')
+    return data;
 }
 export default LandingPage;
