0b6ea20067f93a8e003e7a79b6962d45d1b9d63a
Your Name <you@example.com>
2021-05-08 03:42:21 +0600

    test case for tickets routes

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -4,19 +4,20 @@ import { app } from '../../app';
 it('responds with details about the current user', async () => {
   const cookie = await global.signin();
 
-  const responce = await request(app)
+  const response = await request(app)
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
     .expect(200);
-  // console.log('current-user', responce.body);
-  expect(responce.body.currentUser.email).toEqual('test@test.com');
+
+  expect(response.body.currentUser.email).toEqual('test@test.com');
 });
 
 it('responds with null if not authenticated', async () => {
-  const responce = await request(app)
+  const response = await request(app)
     .get('/api/users/currentuser')
     .send()
-    .expect(401);
-  expect(responce.body.currentUser).toEqual(undefined);
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
 });
--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,9 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@lordjs/tickethub-common';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@lordjs/tickethub-common';
 import { createTicketRouter } from './routes/new';
-
+import { showTicketRouter } from './routes/show';
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -14,9 +18,10 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
 
 app.use(createTicketRouter);
-
+app.use(showTicketRouter);
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
