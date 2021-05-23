9e43bd3a5b25547c12abca02302091e5ee9da0c3
rafiq <rafiq.i@live.co.uk>
2021-05-23 10:57:40 +0600

    publishing event for order created and deleted

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -5,7 +5,8 @@ import {
   NotAuthorizedError,
 } from '@lordjs/tickethub-common';
 import { Order, OrderStatus } from '../models/order';
-
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 const router = express.Router();
 
 router.delete(
@@ -14,7 +15,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if (!order) {
       throw new NotFoundError();
@@ -24,7 +25,13 @@ router.delete(
     }
     order.status = OrderStatus.Cancelled;
     await order.save();
-
+    // publish an event saying  this order was cancelled
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
     res.status(204).send(order);
   }
 );
