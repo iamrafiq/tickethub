b8d9266ac430d4ac90b8919cf7aa8eefef0f2e16
rafiq <rafiq.i@live.co.uk>
2021-05-28 06:39:28 +0600

    Payment created publisher created

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 } from '@lordjs/tickethub-common';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
+import { Payment } from '../models/payment';
 
 const router = express.Router();
 
@@ -33,11 +34,17 @@ router.post(
       throw new BadRequestError('Cannot pay for and cancelled order');
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'usd',
       amount: order.price * 100, // stripe takes smalest units cents, but our price is in usd doller that why we multiply with 100 to make it cents
       source: token,
     });
+
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
     res.send({ success: true });
   }
 );
